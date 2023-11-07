d82b776c5bbb4a869233b7afcc1373f780fa0ab2
nghianguyenbbv <nghia2nguyen@bbv.vn>
2023-11-07 11:34:54 +0700

    dsds

--- example/config.js
+++ example/config.js
@@ -1,11 +1,10 @@
 module.exports = {
-  token:"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", 
   platform: 'github',
   logLevel: 'info',
   automergeType: "pr", 
   onboarding: true,
   "extends": ["github>renovatebot/.github"],
-  repositories: ['nghian2guyenbbv/renovateCircleCiLocal'],
+  repositories: ['nghian2guyenbbv/BE_renovate_bot'],
   gitAuthor: "nghian2nguyenbbv <nghiashinhan96@gmail.com>",
   baseBranches: ["master"],
   "platformAutomerge": false,
