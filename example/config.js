9ea2c45ab11d93688c8c5c6d27cc766c3705de9b
nghianguyenbbv <nghia2nguyen@bbv.vn>
2023-11-07 17:33:25 +0700

    update config js

---
+++ example/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    platform: 'github',
+    logLevel: 'info',
+    automergeType: "pr", 
+    onboarding: true,
+    "extends": ["github>renovatebot/.github"],
+    repositories: ['nghian2guyenbbv/BE_renovate_bot'],
+    gitAuthor: "nghian2nguyenbbv <nghiashinhan96@gmail.com>",
+    baseBranches: ["master"],
+    platformAutomerge: false,
+    dependencyDashboard: true,
+    branchPrefix: "renovate/",
+  };
